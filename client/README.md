276641522ce9ca1196a368518cff59129fb0bd7a
XuyangHao(郝旭阳) <wh2110069@wh.wistronits.com>
2021-10-20 17:52:11 +0800

    初始化

---
+++ client/README.md
@@ -0,0 +1,16 @@
+## Client
+
+后端服务
+
+* client/ 客户端文件目录，存放JavaScript、HTML、CSS等文件
+* node-modules/ 依赖模块
+* server/ 服务端资源目录
+  * boot/ 存放启动脚本，详情请查看官方文档
+  * models/ 模型存放目录
+  * component-config.json 加载组件配置文件
+  * config.json 服务端配置文件
+  * datasources.json 数据源配置文件
+  * middleware.development.json 开发环境中间件配置
+  * middleware.json 默认中间件配置
+  * model-config.json 模型绑定到数据源配置
+  * server.js 服务端程序入口
---
+++ server/models/city.js
@@ -0,0 +1,133 @@
+'use strict';
+
+module.exports = function(city) {
+  // 分页查询(查询为get,所以添加该post方法)
+  city.findList = async function(arg = {}) {
+    try {
+      let {pageSize, pageIndex, name} = arg;
+      let limit = pageSize;
+      let skip = pageIndex * pageSize;
+      if (!(typeof pageIndex == 'number' && pageIndex >= 0)) {
+        throw new Error('pageIndex 不合规');
+      }
+      if (!(typeof pageSize == 'number' && pageSize > 0)) {
+        throw new Error('pageSize 不合规');
+      }
+      let findArg = {
+        // 限制实例数
+        limit,
+        // 跳过实例数
+        skip,
+        // 通过id排序 降序 默认升序
+        // order: 'id DESC',
+      };
+      let countArg = {};
+      if (name) {
+        findArg.where = {name: {like: name}};
+        countArg.where = {name: {like: name}};
+      }
+      let data = await city.find(findArg);
+      let count = await city.count(countArg.where);
+      return {
+        status: true,
+        data,
+        count,
+      };
+    } catch (error) {
+      return {
+        status: false,
+        errMsg: error.message,
+      };
+    }
+  };
+  // 查询一个(查询为get,所以添加该post方法)
+  city.findOneByPost = async function(arg = {}) {
+    try {
+      let one = await city.findOne({
+        // 条件
+        where: {and: [arg]},
+        // 跳过实例数
+        skip: 0,
+        // 限制实例数
+        limit: 0,
+        // 包含在响应的字段或者是去掉的
+        fields: [],
+      });
+
+      return {
+        status: true,
+        data: one,
+      };
+    } catch (error) {
+      return {
+        status: false,
+        errMsg: error.message,
+      };
+    }
+  };
+  // 通过id添加或编辑(可将条件加在body而不用放在链接中)
+  city.insertOrUpdate = async function(arg = {}) {
+    try {
+      let state = await city.upsert(arg);
+      return {
+        status: true,
+        data: state,
+      };
+    } catch (error) {
+      return {
+        status: false,
+        errMsg: error.message,
+      };
+    }
+  };
+  // 通过id删除(删除为delete,,所以添加该post方法)
+  city.delete = async function(arg = {}) {
+    try {
+      let {id} = arg;
+      if (!id) {
+        throw new Error('id 为删除必填参数');
+      }
+      let state = await city.deleteById(id);
+      if (state.count == 0) {
+        throw new Error(`id 为 ${id} 的数据已删除`);
+      }
+      return {
+        status: true
+      };
+    } catch (error) {
+      return {
+        status: false,
+        errMsg: error.message,
+      };
+    }
+  };
+
+  city.remoteMethod('findList', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
+    returns: {
+      root: true,
+      type: 'object',
+    },
+  });
+  city.remoteMethod('findOneByPost', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
+    returns: {
+      root: true,
+      type: 'object',
+    },
+  });
+  city.remoteMethod('insertOrUpdate', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
+    returns: {
+      root: true,
+      type: 'object',
+    },
+  });
+  city.remoteMethod('delete', {
+    accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
+    returns: {
+      root: true,
+      type: 'object',
+    },
+  });
+};
